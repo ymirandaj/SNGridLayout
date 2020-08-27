8dd464bdfbc84f54beb6309cc9970b8c689f5d23
Yomar Miranda <yomarmiranda16@gmail.com>
2020-08-26 22:09:46 -0500

    Agregar archivos de proyecto.

---
+++ SNGridLayout/SNGridLayout/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SNGridLayout.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SNGridLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Home();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
